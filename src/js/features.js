c29367d9d2d9e180ad6f217549e8f8b58b8775f4
YevhenKharchenko <evgenvalerych@gmail.com>
2026-07-28 20:03:23 +0300

    Add sections

--- src/js/features.js
+++ src/js/features.js
@@ -20,7 +20,7 @@ featuresSwiper = new Swiper('.features-swiper-container', {
   breakpoints: {
     1440: {
       centeredSlides: false,
-      slidesPerView: 4,
+      slidesPerView: 5,
       grabCursor: false,
       allowTouchMove: false,
       spaceBetween: 0,
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './js/header.js';
-import './js/advantages.js';
-import './js/characters.js';
+import './js/gameplay.js';
 import './js/features.js';
 import './js/gallery.js';
 import './js/reviews.js';
+import './js/faq.js';
